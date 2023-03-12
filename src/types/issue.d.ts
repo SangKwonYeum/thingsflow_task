cf2e599b707917e3c57927c6f530b2bcd5e2d2ee
Yummy-sk <kunshup2000@gmail.com>
2023-03-12 14:03:30 +0900

    feat: user 타입 추가

--- src/types/issue.d.ts
+++ src/types/issue.d.ts
@@ -37,6 +37,27 @@ export interface Issue {
   state_reason: 'completed' | 'reopened' | 'not_planned' | null;
 }
 
+export interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
 export interface Label {
   id: number;
   node_id: string;
