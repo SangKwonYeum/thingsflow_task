ac97426319fd49beb2042c9d84a270ccbe54281f
Yummy-sk <kunshup2000@gmail.com>
2023-03-12 14:22:24 +0900

    feat: flashlist 반영

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
-import { Text, Button } from 'react-native';
+import React, { useContext, useState, useCallback } from 'react';
+import { RefreshControl, Text, View } from 'react-native';
+import { FlashList } from '@shopify/flash-list';
 import { IssueContext } from 'context/IssueProvider';
 import * as S from './styles';
 
@@ -8,21 +9,54 @@ interface Props {
 }
 
 export function HomeContainer({ onRoute }: Props): JSX.Element {
-  const { data, loading, error, onFetch, page } = useContext(IssueContext);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+  const { data, loading, error, onFetch, page, hasNextPage } =
+    useContext(IssueContext);
 
   const onFetchMore = (): void => {
     onFetch({ page: page + 1 });
   };
 
+  const onRefresh = useCallback((): void => {
+    // NOTE: Refreshing..
+    setRefreshing(true);
+
+    onFetch({ page: 1, needRefresh: true });
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, [onFetch]);
+
   return (
-    <S.Container>
-      <Text>Home</Text>
-      <Button
-        title='hello'
-        onPress={(): void => {
-          onRoute({ id: '1' });
-        }}
-      />
-    </S.Container>
+    <>
+      <S.Container>
+        <FlashList
+          scrollEnabled={loading || !refreshing}
+          scrollEventThrottle={100}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          data={data}
+          estimatedItemSize={120}
+          renderItem={({ item }): JSX.Element => (
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          )}
+          onEndReached={(): void => {
+            if (hasNextPage) {
+              onFetchMore();
+            }
+          }}
+          onEndReachedThreshold={0.5}
+        />
+      </S.Container>
+      {loading && hasNextPage && !refreshing ? (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      ) : null}
+    </>
   );
 }
--- src/components/home/styles.ts
+++ src/components/home/styles.ts
@@ -4,4 +4,6 @@ export const Container = styled.View`
   flex: 1;
   width: 100%;
   background-color: #ffffff;
+
+  padding-top: 20px;
 `;
