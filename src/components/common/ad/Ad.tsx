821b5cc3ed46b03842377f525d88d680ab348e7a
Yummy-sk <kunshup2000@gmail.com>
2023-03-12 15:30:33 +0900

    refactor: 고정 링크 상수로 변경

--- src/components/common/ad/Ad.tsx
+++ src/components/common/ad/Ad.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Linking } from 'react-native';
+import { AD_IMAGE_URL, AD_LINK_URL } from 'constants';
 import * as S from './Ad.styles';
 
 export default function Ad(): JSX.Element {
-  const url =
-    'https://hellobot-test.s3.ap-northeast-2.amazonaws.com/image/01fdd797-0477-4717-8d70-8551150463f7';
-
   const onPress = (): void => {
-    Linking.openURL('https://thingsflow.com/ko/home');
+    Linking.openURL(AD_LINK_URL);
   };
 
   return (
     <S.Container onPress={onPress}>
       <S.Logo
         source={{
-          uri: url,
+          uri: AD_IMAGE_URL,
         }}
       />
     </S.Container>
